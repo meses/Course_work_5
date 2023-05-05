e8753134f00c188044c4f6d58a399fb93f0b7023
Artem <akirillov@itps-russia.ru>
2023-05-05 16:32:51 +0500

    Добавление описания к функциям и добавление функции получения id компаний с hh

--- utils/utils.py
+++ utils/utils.py
@@ -1,43 +1,58 @@
+import requests
+import json
+import time
 
-
-def set_correct_salary_hh(salary_original) -> dict:
-    """Приводит к единому виду зарплату с hh"""
-    if salary_original is None:
-        correct_salary = {"from": 0,
-                          "to": 0,
-                          "currency": "RUR",
-                          "gross": False}
-    elif salary_original['from'] is None:
-        salary_original['from'] = 0
-        correct_salary = salary_original
-    elif salary_original['to'] is None:
-        salary_original['to'] = 0
-        correct_salary = salary_original
-    else:
-        correct_salary = salary_original
-    return correct_salary
 
 def set_correct_salary_from(salary_original):
-    """Приводит к единому виду зарплату с hh"""
+    """Приводит к единому виду зарплату от с hh"""
     if salary_original is None:
         correct_salary_from = None
+    elif salary_original['from'] is None:
+        correct_salary_from = None
     else:
-        correct_salary_from = salary_original['from']
+        correct_salary_from = str(salary_original['from'])
     return correct_salary_from
 
 def set_correct_salary_to(salary_original):
-    """Приводит к единому виду зарплату с hh"""
+    """Приводит к единому виду зарплату до с hh"""
     if salary_original is None:
         correct_salary_to = None
+    elif salary_original['to'] is None:
+        correct_salary_to = None
     else:
-        correct_salary_to = salary_original['to']
+        correct_salary_to = str(salary_original['to'])
     return correct_salary_to
 
 def set_correct_currency(salary_original):
-    """Приводит к единому виду зарплату с hh"""
+    """Приводит к единому виду валюту зарплаты с hh"""
     if salary_original is None:
         correct_currency = None
     else:
-        correct_currency = salary_original['currency']
+        correct_currency = str(salary_original['currency'])
     return correct_currency
+
+def getEmployers():
+    """Функция для получения id компаний с hh"""
+    req = requests.get('https://api.hh.ru/employers')
+    data = req.content.decode()
+    req.close()
+    count_of_employers = json.loads(data)['found']
+    employers = []
+    i = 0
+    j = count_of_employers
+    while i < j:
+        req = requests.get('https://api.hh.ru/employers/' + str(i + 1))
+        data = req.content.decode()
+        req.close()
+        jsObj = json.loads(data)
+        try:
+            employers.append([jsObj['id'], jsObj['name']])
+            i += 1
+            print([jsObj['id'], jsObj['name']])
+        except:
+            i += 1
+            j += 1
+        if i % 200 == 0:
+            time.sleep(0.2)
+    return employers
 
