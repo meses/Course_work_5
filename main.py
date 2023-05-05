8c32689e84ac5ad95c9a7d225e9556874635962c
Artem <akirillov@itps-russia.ru>
2023-05-05 16:31:01 +0500

    Доработка основного меню

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from Classes.DB_Saver import DB_Saver
 from Classes.DB_Manager import DB_Manager
 import sys
 
-emp_id = ['2492', '5974128', '586', '590', '599', '533', '490', '399', '330', '272', '229', '211', '139', '89',
+employers_id = ['2492', '5974128', '586', '590', '599', '533', '490', '399', '330', '272', '229', '211', '139', '89',
               '80', '65', '59']
 def get_employers_info():
     company_list = []
-    for i in emp_id:
+    for i in employers_id:
         test_company = HeadHunter().get_company_info(i)
         company_list.append(test_company)
 
@@ -16,7 +16,7 @@ def get_employers_info():
 
 def get_vacancies_info():
     vacancies_list = []
-    test_company = HeadHunter().get_requests(emp_id)
+    test_company = HeadHunter().get_requests(employers_id)
     for i in test_company['items']:
         vacancies_list.append(i)
 
@@ -31,6 +31,7 @@ if is_update_employers_data == '1':
     DB_Saver().write_vacancies(get_vacancies_info())
 
 def main_coise():
+    """Пользовательский выбор запроса к БД"""
     print('')
     main_menu_coise = input('Выберите, какую информацию хотите получить:\n1 - Cписок всех компаний и количество вакансий у каждой компании.\n'
                         '2 - Список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию.\n'
@@ -41,31 +42,34 @@ def main_coise():
     return main_menu_coise
 
 main_menu_coise = main_coise()
-if main_menu_coise == '1':
-    info = DB_Manager().get_companies_and_vacancies_count()
-    for i in info:
-        print(i)
-    main_coise()
-elif main_menu_coise == '2':
-    info = DB_Manager().get_all_vacancies()
-    for i in info:
-        print(i)
-    main_coise()
-elif main_menu_coise == '3':
-    info = DB_Manager().get_avg_salary()
-    print(f'Средняя зарплата от: {info[0]},\nСредняя зарплата до: {info[1]}')
-    main_coise()
-elif main_menu_coise == '4':
-    info = DB_Manager().get_vacancies_with_higher_salary()
-    for i in info:
-        print(i)
-    main_coise()
-elif main_menu_coise == '5':
-    vacansy_keyword = input('Введите название вакансии, которую хотите найти: ')
-    info = DB_Manager().get_vacancies_with_keyword(vacansy_keyword)
-    for i in info:
-        print(i)
-    main_coise()
-elif main_menu_coise == '0':
-    sys.exit()
+while main_menu_coise != '0':
+    if main_menu_coise == '1':
+        info = DB_Manager().get_companies_and_vacancies_count()
+        for i in info:
+            print(i)
+        main_menu_coise = main_coise()
+    elif main_menu_coise == '2':
+        info = DB_Manager().get_all_vacancies()
+        for i in info:
+            print(i)
+        main_menu_coise = main_coise()
+    elif main_menu_coise == '3':
+        info = DB_Manager().get_avg_salary()
+        print(f'Средняя зарплата от: {info[0]}\nСредняя зарплата до: {info[1]}')
+        main_menu_coise = main_coise()
+    elif main_menu_coise == '4':
+        info = DB_Manager().get_vacancies_with_higher_salary()
+        for i in info:
+            print(i)
+        main_menu_coise = main_coise()
+    elif main_menu_coise == '5':
+        vacansy_keyword = input('Введите название вакансии, которую хотите найти: ')
+        info = DB_Manager().get_vacancies_with_keyword(vacansy_keyword)
+        for i in info:
+            print(i)
+        main_menu_coise = main_coise()
+    elif main_menu_coise == '0':
+        sys.exit()
+    else:
+        main_menu_coise = main_coise()
 
